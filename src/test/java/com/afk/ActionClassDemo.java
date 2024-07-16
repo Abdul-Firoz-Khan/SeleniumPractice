946a7f7ae9a248b7ed2f4400b864ddc6bb394d53
Abdul-Firoz-Khan <165898558+Abdul-Firoz-Khan@users.noreply.github.com>
2024-07-16 18:05:08 +0530

    Second Commit

--- src/test/java/com/afk/ActionClassDemo.java
+++ src/test/java/com/afk/ActionClassDemo.java
@@ -1,9 +1,8 @@
+package com.afk;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
